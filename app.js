bd2aae6cf44bd30fbe13d9e161eeccf3ca5a904d
이상우 <samkolder0102@gmail.com>
2025-02-21 18:15:25 +0900

    Drawing House

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const canvas = document.querySelector("canvas"); //js로 canvas 불러오기
-const cxt = canvas.getContext("2d");  //context는 캔버스에 그림을 그릴 떄 사용하는 붓
+const cxt = canvas.getContext("2d"); //context는 캔버스에 그림을 그릴 떄 사용하는 붓
 canvas.width = 800;
-canvas.height = 800;  //css에서 캔버스 크기 설정 후 js에도 작성해줌(이후에는 js에서만 수정할 것임)
+canvas.height = 800; //css에서 캔버스 크기 설정 후 js에도 작성해줌(이후에는 js에서만 수정할 것임)
 
-cxt.moveTo(50, 50); //선을 긋지 않고 연필(좌표)을 이동시킴
-cxt.lineTo(150, 50);  //선을 그으면서 연필을 이동시킴
-cxt.lineTo(150, 150); //수평인 직선을 그으려면 두 y값이 같아야함
-cxt.lineTo(50, 150);  //라인이 끝난 지점이 다음에 시작하는 브러쉬 좌표임
-cxt.lineTo(50, 50); //fillReact = fill + Rect = fill + (moveTo + lineTo)
+cxt.fillRect(200, 200, 50, 200); //왼쪽 벽 만들기
+cxt.fillRect(400, 200, 50, 200); //오른쪽 벽 만들기
+cxt.lineWidth = 2; //선 굵기 조절
+cxt.fillRect(300, 300, 50, 100); //문 만들기
+cxt.fillRect(200, 200, 200, 20); //천장 만들기
+cxt.moveTo(200, 200); //지붕 만들기 위해 연필 이동
+cxt.lineTo(325, 100);
+cxt.lineTo(450, 200);
 cxt.fill();
-cxt.stroke();
