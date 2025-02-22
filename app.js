f74519cfffee93862832d449192842bcae20909c
이상우 <samkolder0102@gmail.com>
2025-02-22 13:37:10 +0900

    Line Width

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas"); //js로 canvas 불러오기
 const ctx = canvas.getContext("2d"); //context는 캔버스에 그림을 그릴 떄 사용하는 붓
 canvas.width = 800;
 canvas.height = 800; //css에서 캔버스 크기 설정 후 js에도 작성해줌(이후에는 js에서만 수정할 것임)
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value; //굵기 초기값 5 주겠다는 의미
 let isPainting = false;
 
 function onMove(event) {
@@ -12,6 +13,7 @@ function onMove(event) {
     ctx.stroke();
     return;
   }
+  ctx.beginPath(); //경로 새시작하여 이전 선들의 굵기 유지시킴
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -24,8 +26,14 @@ function cancelPainting() {
   //마우스 클릭상태 해제시 그리기 중단 함수
   isPainting = false;
 }
+function onLineWidthChange(event) {
+  //굵기 바꾸는 함수
+  ctx.lineWidth = event.target.value;
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting); //커서가 캔버스 이탈시 그리기 중단
+
+lineWidth.addEventListener("change", onLineWidthChange);
